686227bc88101281a5e8035d55cffc5305a1d7dc
Pavaleanu Ciprian <pavaleanuciprian@yahoo.com>
2024-09-05 13:24:06 +0300

    Set off a broken animation

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -8,17 +8,6 @@ const HowItWorks = () => {
   const videoRef = useRef();
 
   useGSAP(() => {
-    gsap.from("#chip", {
-      scrollTrigger: {
-        trigger: "#chip",
-        start: "20% bottom",
-      },
-      opacity: 0,
-      scale: 2,
-      duration: 2,
-      ease: "power2.inOut",
-    });
-
     animateWithGsap(".g_fadeIn", {
       opacity: 1,
       y: 0,
